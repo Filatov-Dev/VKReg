eb34fad80045fbc3518172bddc5bcd4befe5f863
Filatov_Yurii <filatov.ios.dev@gmail.com>
2022-03-21 20:57:23 +0300

    Start prod

---
+++ VK/VK/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VK
+//
+//  Created by Юрий Филатов on 21.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+
+
+}
+
